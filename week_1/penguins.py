5fe62a853f9929f0820338c7478e87516a8f0018
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2022-02-05 21:27:21 +0200

    Done

---
+++ week_1/penguins.py
@@ -0,0 +1,13 @@
+a1 = '   _~_    '
+a2 = '  (o o)   '
+a3 = ' /  V  \  '
+a4 = '/(  _  )\ '
+a5 = '  ^^ ^^   '
+
+count = int(input())
+
+print(a1 * count)
+print(a2 * count)
+print(a3 * count)
+print(a4 * count)
+print(a5 * count)
